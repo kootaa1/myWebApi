5c4a07e30e43a9dee07ee22896a42fc06e5a688b
kootaa1 <kootaa1488@gmail.com>
2019-10-16 11:01:43 +0700

    Добавьте файлы проекта.

---
+++ MyWebAPI/Controllers/TodoController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using MyWebAPI.Models;
+using System.Web.Http.Cors;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MyWebAPI.Controllers
+{
+    [Route("api/Todos")]
+    public class TodoController : Controller
+    {
+
+        public TodoController()
+        {
+            storage.Add(new TodoItem
+            {
+                Id = 1,
+                IsComplete = true,
+                Name = "Do something 1"
+            });
+            storage.Add(new TodoItem
+            {
+                Id = 2,
+                IsComplete = false,
+                Name = "Do something 2"
+            });
+            storage.Add(new TodoItem
+            {
+                Id = 3,
+                IsComplete = true,
+                Name = "Do something 3"
+            });
+        }
+        //TodoTempStorage storage = new TodoTempStorage();
+        List<TodoItem> storage = new List<TodoItem>();
+
+        [HttpGet]
+        public async Task<ActionResult<List<TodoItem>>> GetTodoItems()
+        {
+            if (storage == null)
+                return NotFound();
+            return storage;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = storage.Find(x => x.Id == id);
+            if (todoItem == null)
+                return NotFound();
+            return todoItem;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            if (todoItem == null)
+            {
+                return BadRequest();
+            }
+            if (storage.Find(x => x == todoItem) == null)
+            {
+                return BadRequest();
+            }
+            todoItem.Id = storage.Capacity + 1;
+            todoItem.IsComplete = false;
+            storage.Add(todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if (id != item.Id)
+                return BadRequest();
+            int index = storage.FindIndex(x => x == item);
+            if (index != -1)
+            {
+                storage[index].Id = item.Id;
+                storage[index].Name = item.Name;
+                storage[index].IsComplete = item.IsComplete;
+                return NoContent();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TodoItem>> DeleteTodoItem(long id)
+        {
+            var todoItemIndex = storage.FindIndex(x => x.Id == id);
+            if (todoItemIndex == -1)
+            {
+                return NotFound();
+            }
+            TodoItem DeletedItem = storage[todoItemIndex];
+            storage.Remove(storage[todoItemIndex]);
+            return DeletedItem;
+        }
+    }
+}
---
+++ MyWebAPI/Models/TodoTempStorage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MyWebAPI.Models
+{
+    public class TodoTempStorage
+    {
+        private List<TodoItem> TodoStorage = new List<TodoItem>();
+
+        public bool AddItem(TodoItem item)
+        {
+            if (TodoStorage.Find(x => x == item) == null)
+            {
+                TodoStorage.Add(item);
+                return true;
+            }
+            return false;
+        }
+
+        public TodoItem GetItem(long ItemId)
+        {
+            return TodoStorage.Find(x => x.Id == ItemId);
+        }
+
+        public List<TodoItem> GetItems()
+        {
+            return TodoStorage;
+        }
+
+        public bool UpdateItem(TodoItem item)
+        {
+            int index = TodoStorage.FindIndex(x => x == item);
+            if (index != -1)
+            {
+                TodoStorage[index].Id = item.Id;
+                TodoStorage[index].Name = item.Name;
+                TodoStorage[index].IsComplete = item.IsComplete;
+                return true;
+            }
+            return false;
+        }
+
+        public bool DeleteItem(long ItemId)
+        {
+            int index = TodoStorage.FindIndex(x => x.Id == ItemId);
+            if (index != -1)
+            {
+                TodoStorage.Remove(TodoStorage[index]);
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
